6364bc1535229d779d59b02cd15bb8d6bcc46881
ha1t <halt.feits@gmail.com>
2013-06-06 18:10:01 +0900

    little fix

--- example.php
+++ example.php
@@ -7,7 +7,9 @@
 require_once dirname(__FILE__) . '/ExceptionDumper.php';
 
 try {
+    eval("
     throw new Exception('this is error!!!');
+        ");
 } catch (Exception $e) {
     echo ExceptionDumper::fetch($e);
 }
